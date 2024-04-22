814e3640f96f23e3169127bc21ff591a8a869e41
Jordan Palmer <jordanpalmer@Jordans-MacBook-Pro-3.local>
2024-04-22 10:14:34 +0100

    small changes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def update_plot():
     price_set = float(request.args.get("mean_value", 0))
     # global mean_value
     x = time.time() * 1000  # x-axis: time in milliseconds. Doesnt get used
-
+    
     n_customers = customers.n_customers(10, 100)
     max_prices = customers.maximum_prices(n_customers, max_price_mean=50, max_price_std=3)
 
@@ -24,7 +24,8 @@ def update_plot():
     reward = customers.revenue_per_look(n_customers, price_set, max_prices)
     ### get reward from the RL price
     # need to create qtable if first iteration. Can implement a counter in index.html and request it here?
-
+    if price_set == 0:
+        reward = 0
     # z is a placeholder to put the RL reward
     return jsonify(x=x, y=reward, z=1)
 
@@ -40,6 +41,7 @@ def execute_python_code():
     code = unquote(data.get("code"))
     xData = data.get("xData")
     yData = data.get("yData")
+    prices = data.get("prices")
     print("code = ", code)
     print("xData = ", xData)
     print("yData = ", yData)
@@ -52,7 +54,7 @@ def execute_python_code():
             # Add functions to the dictionary
             func_dict['set_price'] = obj
 
-    result = func_dict['set_price'](xData, yData)
+    result = func_dict['set_price'](xData, yData, prices)
     # Do something with the code
     return jsonify({"result": result})
 
