b40482fe00911464af718eac98ee83bb0c6616a0
Jordan Palmer <jordanpalmer@Jordans-MacBook-Pro-3.local>
2024-05-09 08:14:24 +0100

    flask needs pyodide

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import customers as customers
 import logging
 from urllib.parse import unquote
 import json
+import subprocess
 
 app = Flask(__name__, static_folder='static/')
 
@@ -40,28 +41,52 @@ def set_mean(mean):
     mean_value = mean
     return "Mean set to {:.2f}".format(mean)
 
-@app.route("/execute_python_code", methods=["POST"])
-def execute_python_code():
-    data = request.json  # Parse JSON data from request body
-    code = unquote(data.get("code"))
-    xData = data.get("xData")
-    yData = data.get("yData")
-    prices = data.get("prices")
-    print("code = ", code)
-    print("xData = ", xData)
-    print("yData = ", yData)
-    # run python code
-    func_dict = {}
-    exec(code, globals())
-    print("executed code")
-    for name, obj in globals().items():
-        if name == 'set_price':
-            # Add functions to the dictionary
-            func_dict['set_price'] = obj
+# @app.route("/execute_python_code", methods=["POST"])
+# def execute_python_code():
+#     data = request.json  # Parse JSON data from request body
+#     code = unquote(data.get("code"))
+#     xData = data.get("xData")
+#     yData = data.get("yData")
+#     prices = data.get("prices")
+#     print("code = ", code)
+#     print("xData = ", xData)
+#     print("yData = ", yData)
+#     # run python code
+#     func_dict = {}
+#     exec(code, globals())
+#     print("executed code")
+#     for name, obj in globals().items():
+#         if name == 'set_price':
+#             # Add functions to the dictionary
+#             func_dict['set_price'] = obj
 
-    result = func_dict['set_price'](xData, yData, prices)
-    # Do something with the code
-    return jsonify({"result": result})
+#     result = func_dict['set_price'](xData, yData, prices)
+#     # Do something with the code
+#     return jsonify({"result": result})
+
+@app.route('/execute_python_code', methods=['POST'])
+def execute_python_code():
+    import os
+    import sys
+
+    # Add the directory containing the PyPy executable to the PATH
+    pypy_path = "/Users/jordanpalmer/Downloads/pypy3.10-v7.3.16-macos_arm64/bin/pypy3"  # Replace this with the actual path to PyPy
+    os.environ["PATH"] += os.pathsep + pypy_path
+
+    # Get the Python code from the request
+    code = request.json.get('code', '')
+    
+    try:
+        # Execute Python code using PyPy
+        result = subprocess.run(['pypy3', '-c', code], capture_output=True, text=True)
+        output = result.stdout
+        success = True
+    except Exception as e:
+        output = str(e)
+        success = False
+
+    # Return the result as JSON
+    return jsonify({'output': output, 'success': success})
 
 if __name__ == "__main__":
     app.run(debug=True)
